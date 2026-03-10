ae375de663a0daefa95849bb7f2a4d32b2144ff3
stewartbrown-prog <stewartdavidbrown@googlemail.com>
2026-03-10 20:24:21 +0000

    Create Program.cs with minimal ASP.NET Core web application

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => $"Current Date and Time (UTC - {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss})");
+
+app.Run();
